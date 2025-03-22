3f4366b8087c7139936f7b3c18e8a8481f5faa10
ajshan23 <ajmalshahan23@gmail.com>
2025-03-22 14:57:54 +0530

    edited and fixed bugs and final push before auth

--- src/controllers/pdfController.js
+++ src/controllers/pdfController.js
@@ -388,7 +388,7 @@ export const generatePdf = async (req, res) => {
         }
 
         .bottom_div div p {
-            font-size: 11px;
+            font-size: 10px;
             color: black;
             margin: 0;
         }
@@ -1057,7 +1057,7 @@ export const generatePdfById = async (req, res) => {
               position: relative;
             }
             .bottom_div div p {
-              font-size: 11px;
+              font-size: 10px;
               color: black;
               margin: 0;
             }
